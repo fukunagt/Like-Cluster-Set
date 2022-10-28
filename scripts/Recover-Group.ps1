1bc665afe6179d47e648b05888315714404e238f
fukunagt <t-fukunaga@nec.com>
2022-10-28 09:31:59 +0900

    Modify script to get the group status.

--- scripts/Recover-Group.ps1
+++ scripts/Recover-Group.ps1
@@ -14,24 +14,49 @@
 #------------------------------------------------------------
 # clusters
 # - Add cluster servers.
-# - The first cluster number is 0 and the second one is 1.
+# - The following sample has 2 clusters.
+# - If you want to add one more cluster that has server5 and
+#   server6, write as below.
+#$clusters = @(
+#    @(
+#        @("server1", "192.168.1.1", "29009", "Administrator", "Passw0rd"),
+#        @("server2", "192.168.1.2", "29009", "Administrator", "Passw0rd")
+#    ),
+#    @(
+#        @("server3", "192.168.1.3", "29009", "Administrator", "Passw0rd"),
+#        @("server4", "192.168.1.4", "29009", "Administrator", "Passw0rd")
+#    ),
+#    @(
+#        @("server5", "192.168.1.5", "29009", "Administrator", "Passw0rd"),
+#        @("server6", "192.168.1.6", "29009", "Administrator", "Passw0rd")
+#    )
+#)
 #------------------------------------------------------------
 $clusters = @(
     @(
-        @("server1", "192.168.1.1", "29009", "Administrator", "password"),
-        @("server2", "192.168.1.2", "29009", "Administrator", "password")
+        @("server1", "192.168.1.1", "29009", "Administrator", "Passw0rd"),
+        @("server2", "192.168.1.2", "29009", "Administrator", "Passw0rd")
     ),
     @(
-        @("server3", "192.168.1.3", "29009", "Administrator", "password"),
-        @("server4", "192.168.1.4", "29009", "Administrator", "password")
+        @("server3", "192.168.1.3", "29009", "Administrator", "Passw0rd"),
+        @("server4", "192.168.1.4", "29009", "Administrator", "Passw0rd")
     )
 )
+
 #------------------------------------------------------------
 # groups
 # - Add failover group in the clusters.
+# - The following sample has 2 groups.
+# - If you want to add one more group, write as below.
+#$groups = @(
+#    @("failover1"),
+#    @("failover2"),
+#    @("failover3")
+#)
 #------------------------------------------------------------
 $groups = @(
-    @("failover1")
+    "failover1",
+    "failover2"
 )
 #============================================================
 
@@ -39,13 +64,11 @@ $groups = @(
 #============================================================
 # You don't need to change the following lines.
 #============================================================
-
-# FIXME:
-# This is for test. You need to get the actual server name.
-$hostname = "server1"
+# Get the current hostname.
+$hostname = hostname
 
 # Find my server in the clusters matrix.
-$clusternumber = -1
+$clusterid = -1
 Write-Debug $clusters.Length
 for ($i = 0; $i -lt $clusters.Length; $i++)
 {
@@ -55,18 +78,20 @@ for ($i = 0; $i -lt $clusters.Length; $i++)
         Write-Debug $clusters[$i][$j][0]
         if ($clusters[$i][$j][0] -eq $hostname)
         {
-            $clusternumber = $i
-            Write-Output "$hostname is in cluster $clusternumber."
+            $clusterid = $i
+            $serverid = $j
+            Write-Output "$hostname is in cluster ID: $clusterid."
+            Write-Output "$hostname is server ID: $serverid."
             break;
         }
     }
-    if ($clusternumber -ne -1)
+    if ($clusterid -ne -1)
     {
         break;
     }
 }
-Write-Debug $clusternumber
-if ($clusternumber -eq -1)
+Write-Debug $clusterid
+if ($clusterid -eq -1)
 {
     Write-Output "Cannot find the server in the cluster matrix."
     # FIXME: I need to add clplogcmd to show some message.
@@ -79,8 +104,63 @@ for ($i = 0; $i -lt $groups.Length; $i++)
 {
     Write-Debug $groups[$i]
     
+    # Get the group status from my API server.
+    $user = $clusters[$clusterid][$serverid][3]
+    $pass = $clusters[$clusterid][$serverid][4]
+    $uri = "http://" + $clusters[$clusterid][$serverid][1] + ":" + $clusters[$clusterid][$serverid][2] + "/api/v1/groups/failover1"
+    Write-Output $user
+    Write-Debug $pass
+    Write-Output $uri
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$pass)))
+    $ret = Invoke-RestMethod -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Uri $uri
+    Write-Output $ret.groups.status
+    if ($ret.groups.status -eq "Online")
+    {
+        $group = $groups[$i]
+        $current = $ret.groups.current
+        Write-Output "$group is running on $current."
+        break;
+    }
     # FIXME
-    # I need to add scripts to do followings.
-    # - Run curl command to check the status.
-    # - Run curl command to start the group.
+    # - I need to consider the other status (pending, failure)
+
+    # Get the group status from API server on the other clusters.
+    for ($j = 0; $j -lt $clusters.Length; $j++)
+    {
+        if ($j -eq $clusterid)
+        {
+            # Do nothing
+        }
+        else
+        {
+            for ($k = 0; $k -lt $clusters[$k].Length; $k++)
+            {
+                Write-Output $clusters[$j][$k][0]
+                $user = $clusters[$j][$k][3]
+                $pass = $clusters[$j][$k][4]
+                $uri = "http://" + $clusters[$j][$k][1] + ":" + $clusters[$j][$k][2] + "/api/v1/groups/failover1"
+                Write-Output $user
+                Write-Debug $pass
+                Write-Output $uri
+                $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$pass)))
+                $ret = Invoke-RestMethod -Method Get -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Uri $uri
+                Write-Output $ret.groups.status
+                if ($ret.groups.status -eq "Online")
+                {
+                    $group = $groups[$i]
+                    $current = $ret.groups.current
+                    Write-Output "$group is running on $current."
+                    break;
+                }
+            }
+            # FIXME
+            # - Need to consider the other status (pending, failure)
+        }        
+    }
+    # FIXME
+    # - Need to add some log? 
+    #   - e.g., failover1 is not running on any server.
+
+    # Recover the group
+
 }
